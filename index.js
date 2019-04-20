3888612dd5b7b9e81ea28ede5d409770e29c696c
brittjohn87 <brittanytjohnson@icloud.com>
2019-04-19 22:10:43 -0400

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 function tipCalculator(){
   // Declare Variables using the id attribute tied to the html //
-    var billAmount = document.getElementById('billAmount').value;
-    var serviceQuality = document.getElementById('serviceQuality').value;
-    var guests = document.getElementById('guests').value;
+    var billAmount;
+    var serviceQuality;
+    var guests;
 
   // Declare Variables for our math logic //
-    var tipTotal = billAmount * serviceQuality
-    var billTotal = Number(billAmount) + Number(tipTotal)
+    var tipTotal;
+    var billTotal;
 
   // More than one guest logic //
-    var tipEach = tipTotal / guests
-    var billEach = billAmount / guests
-    var billPerPerson = Number(tipEach) + Number(billEach);
+    var tipEach;
+    var billEach;
+    var billPerPerson;
    
   //  This is where we will display our values //
     $('#tip').html(`$${tipTotal.toFixed(2)}`)
     $('#billTotal').html(`$${billTotal.toFixed(2)}`)
-    guests <= 1 ? $('#billPerPerson').html(`$${billTotal.toFixed(2)}`) : $('#billPerPerson').html(`$${billPerPerson.toFixed(2)}`);
+    
+    // guests <= 1 ? $('#billPerPerson').html(`$${billTotal.toFixed(2)}`) : $('#billPerPerson').html(`$${billPerPerson.toFixed(2)}`);
 }
   
 // calculate button //
 document.getElementById('calculate').onclick = function(){
-  tipCalculator();
+  // here we'll call our calculator function
 };
 
 // function for clearing calculator //
@@ -36,6 +37,6 @@ function clearCalculator(){
 
 // clear button //
 document.getElementById('clear').onclick = function(){
-  clearCalculator();
+  // here we'll call our clear function
 }
 
