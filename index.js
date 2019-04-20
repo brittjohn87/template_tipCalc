da866ce36f65c97c71f1a5dc50c2d5fe230a0468
brittjohn87 <brittanytjohnson@icloud.com>
2019-04-19 22:14:27 -0400

    minor fixes

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function tipCalculator(){
-  // Declare Variables using the id attribute tied to the html //
+  // Declare Variables using the id attribute tied to the html inputs
     var billAmount;
     var serviceQuality;
     var guests;
 
-  // Declare Variables for our math logic //
+  // Declare Variables for our tip and bill total logic
     var tipTotal;
     var billTotal;
 
-  // More than one guest logic //
+  // More than one guest logic
     var tipEach;
     var billEach;
     var billPerPerson;
    
-  //  This is where we will display our values //
+  //  This is where we will display our values 
     $('#tip').html(`$${tipTotal.toFixed(2)}`)
     $('#billTotal').html(`$${billTotal.toFixed(2)}`)
     
     // guests <= 1 ? $('#billPerPerson').html(`$${billTotal.toFixed(2)}`) : $('#billPerPerson').html(`$${billPerPerson.toFixed(2)}`);
 }
   
-// calculate button //
+// calculate button 
 document.getElementById('calculate').onclick = function(){
   // here we'll call our calculator function
 };
 
-// function for clearing calculator //
+// function for clearing calculator
 function clearCalculator(){
   $('#billAmount').val('')
   $('#serviceQuality option:first').prop('selected',true);
@@ -35,7 +35,7 @@ function clearCalculator(){
   $('#billPerPerson').html('')
 }
 
-// clear button //
+// clear button
 document.getElementById('clear').onclick = function(){
   // here we'll call our clear function
 }
